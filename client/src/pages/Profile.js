fed6dca972cdd72cfaa5cec018654f5933eb2a98
Aaron Lisker <aelisker@gmail.com>
2020-07-28 17:13:09 -0700

    added form change and submit handlers to profile and homepage

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,12 +3,16 @@ import { useParams, Redirect } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   const { username: userParam } = useParams();
   console.log(userParam);
 
@@ -17,6 +21,16 @@ const Profile = () => {
   });
 
   const user = data?.me || data?.user || {};
+
+  const handleClick = async() => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   // redirect to personal profile page if username is the logged in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username.toLowerCase() === `${userParam ? userParam.toLowerCase() : ''}`) {
@@ -41,6 +55,12 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+
+      {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+        Add Friend
+      </button>
+      )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -55,6 +75,9 @@ const Profile = () => {
             friends={user.friends}
           />
         </div>
+      </div>
+      <div className="mb-3">
+        {!userParam && <ThoughtForm />}
       </div>
     </div>
   );
